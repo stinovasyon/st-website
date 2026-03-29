88b506974474e22941d71e82720a7ead83b8ea8b
stinovasyon <stinovasyon@proton.me>
2026-03-29 19:22:47 +0000

    vite-plugin-devtools-json package added

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		watch: {
-			usePolling: true
-		}
-	}
+	plugins: [sveltekit(), devtoolsJson()],
+	server: { watch: { usePolling: true } }
 });
